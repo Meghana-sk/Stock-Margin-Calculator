a7febd821d6c04bcd9b34ca6e9851b634213ba1d
Meghana-Sk <meghana.meghanask.sk@gmail.com>
2021-09-19 14:41:03 +0530

    fix: Corrected logic of profit loss margin

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-
 const boughtStockPrice = document.querySelector("#stock-price");
 const quantityBought = document.querySelector("#stocks-quantity");
 const currentStockPrice = document.querySelector("#current-price");
-const checkBtn = document.querySelector("#check-btn")
-const displayResult = document.querySelector("#display-result")
-const lossImage = document.querySelector("#image-loss")
-
+const checkBtn = document.querySelector("#check-btn");
+const displayResult = document.querySelector("#display-result");
+const lossImage = document.querySelector("#image-loss");
 
 function clickHandler() {
-
-  let boughtPrice = Number(boughtStockPrice.value)
+  let boughtPrice = Number(boughtStockPrice.value);
   let quantityStocks = Number(quantityBought.value);
   let currentPrice = Number(currentStockPrice.value);
 
   if (boughtPrice > 0 && quantityStocks > 0 && currentPrice > 0) {
+    let boughtPriceTotalValue = boughtPrice * quantityStocks;
+    let currentPriceTotalValue = currentPrice * quantityStocks;
+    let margin = (
+      ((currentPriceTotalValue - boughtPriceTotalValue) /
+        boughtPriceTotalValue) *
+      100
+    ).toFixed(2);
 
-  let boughtPriceTotalValue = boughtPrice * quantityStocks;
-  let currentPriceTotalValue = currentPrice * quantityStocks;
-  let margin = (((currentPriceTotalValue - boughtPriceTotalValue) / boughtPriceTotalValue) * 100).toFixed(2);
-  
-  if (margin <= -50) {
-    displayResult.style.display = "inline-block";
-    displayResult.innerText = `Ohooo 😞 P/L 🔻${margin}% You lost Rs.${-(currentPriceTotalValue - boughtPriceTotalValue)}`;
-    lossImage.style.display = "block";
-  } else if (margin === 0) {
-    displayResult.style.display = "inline-block";
-    displayResult.innerText = `😐 P/L ${margin}% You gained Rs.${(currentPriceTotalValue - boughtPriceTotalValue)}`;
-    lossImage.style.display = "none";
-  } else if (margin > 0) {
-    document.body.style.backgroundImage = "url()";
-    displayResult.style.display = "inline-block";
-    displayResult.innerText = `🥳 Hurray.P/L 🔼${margin}%. You gained Rs.${(currentPriceTotalValue - boughtPriceTotalValue)}`;
-    lossImage.style.display = "none";
+    if (margin <= -50) {
+      displayResult.style.display = "inline-block";
+      displayResult.innerText = `Ohooo 😞 P/L 🔻${-margin}% You lost Rs.${-(
+        currentPriceTotalValue - boughtPriceTotalValue
+      )}`;
+      lossImage.style.display = "block";
+    } else if (boughtPriceTotalValue === currentPriceTotalValue) {
+      displayResult.style.display = "inline-block";
+      displayResult.innerText = "No gain no loss 😐 ";
+      lossImage.style.display = "none";
+    } else if (margin > 0) {
+      document.body.style.backgroundImage = "url()";
+      displayResult.style.display = "inline-block";
+      displayResult.innerText = `🥳 Hurray.P/L 🔼${margin}%. You gained Rs.${
+        currentPriceTotalValue - boughtPriceTotalValue
+      }`;
+      lossImage.style.display = "none";
+    } else {
+      document.body.style.backgroundImage = "url()";
+      displayResult.style.display = "inline-block";
+      displayResult.innerText = `Loss. P/L 🔻${-margin}% You lost Rs.${-(
+        currentPriceTotalValue - boughtPriceTotalValue
+      )}`;
+      lossImage.style.display = "none";
+    }
   } else {
-    document.body.style.backgroundImage = "url()";
     displayResult.style.display = "inline-block";
-    displayResult.innerText = `Loss. P/L 🔻${margin}% You lost Rs.${-(currentPriceTotalValue - boughtPriceTotalValue)}`;
-    lossImage.style.display = "none";
+    displayResult.innerText = "Given data is invalid 🙃";
   }
 }
-else {
-  displayResult.style.display = "inline-block";
-    displayResult.innerText = "Given data is invalid 🙃"
-}
-}
 
-checkBtn.addEventListener("click", clickHandler)
-
+checkBtn.addEventListener("click", clickHandler);
